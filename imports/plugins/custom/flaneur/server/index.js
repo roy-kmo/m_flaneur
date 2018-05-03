fc591f200abc0ca32092b5e405113c8333024b47
Daniel Castellon <dan.a.castellon@gmail.com>
2018-05-03 16:03:37 -0400

    FLAN-41 Added Intercom.io messenger

--- imports/plugins/custom/flaneur/server/index.js
+++ imports/plugins/custom/flaneur/server/index.js
@@ -1,3 +1,9 @@
+import { BrowserPolicy } from "meteor/browser-policy-common";
+
 import './methods/flaneurMethods';
 import './init';
 import '../lib/schemas/productsSchema';
+
+BrowserPolicy.content.allowOriginForAll("https://widget.intercom.io:*");
+BrowserPolicy.content.allowOriginForAll("https://js.intercomcdn.com:*");
+BrowserPolicy.content.allowOriginForAll("https://static.intercomassets.com:*");
