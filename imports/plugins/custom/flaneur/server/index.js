53fe798a7a81ccaa9e2df3907ce5de7f29debbe5
Daniel Castellon <dan.a.castellon@gmail.com>
2018-06-27 11:04:19 -0400

    FLAN-147 Fix for some browsers blocking local images

--- imports/plugins/custom/flaneur/server/index.js
+++ imports/plugins/custom/flaneur/server/index.js
@@ -12,3 +12,8 @@ BrowserPolicy.content.allowOriginForAll("https://js.intercomcdn.com:*");
 BrowserPolicy.content.allowOriginForAll("https://static.intercomassets.com:*");
 BrowserPolicy.content.allowOriginForAll("https://aktively.us3.list-manage.com:*");
 BrowserPolicy.content.allowOriginForAll("https://fr-assets.com:*");
+
+const absoluteUrl = Meteor.absoluteUrl().slice(0, -1);
+if (absoluteUrl.includes('localhost') === false) {
+  BrowserPolicy.content.allowOriginForAll(`${absoluteUrl}:*`);
+}
