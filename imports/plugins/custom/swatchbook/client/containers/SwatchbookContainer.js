b8c975b091990a6f02b32d8ee97bfa366dc7e979
Benjamin Schmidt <benjaminschmidt@Benjamins-MacBook-Pro-2.local>
2018-07-02 18:06:53 -0400

    added close button to swatchbook

--- imports/plugins/custom/swatchbook/client/containers/SwatchbookContainer.js
+++ imports/plugins/custom/swatchbook/client/containers/SwatchbookContainer.js
@@ -41,12 +41,29 @@ class SwatchbookContainer extends Component {
     ReactionRouter.go('/design-your-bedding');
   };
 
+  handleSwatchViewClick = e => {
+    e.preventDefault();
+    const { isOpen } = this.state;
+    const swatchbookDrawer = $('.swatchbook-drawer-container');
+
+    if (isOpen) {
+      swatchbookDrawer.hide();
+    } else {
+      swatchbookDrawer.show();
+    }
+
+    this.setState({ isOpen: !isOpen });
+  };
+
+
   render () {
     const { colors } = this.state;
     return (
 
       <div className="swatchbook-drawer-container" ref={ref => this.container = ref}>
-
+      <button
+        className="rui btn btn-default flat button swatchbook-link"
+        onClick={this.handleSwatchViewClick}><i class="fas fa-times"></i></button>
           <div className="cart-drawer-swiper-slide swatch">
 
           {!colors.length && (
