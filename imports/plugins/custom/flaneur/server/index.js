12272157cb269f4464f9ca449b3bf29dbf621a99
Benjamin Schmidt <benjaminschmidt@Benjamins-MacBook-Pro-2.local>
2018-07-06 20:52:15 -0400

    update csp

--- imports/plugins/custom/flaneur/server/index.js
+++ imports/plugins/custom/flaneur/server/index.js
@@ -12,6 +12,7 @@ BrowserPolicy.content.allowOriginForAll("https://js.intercomcdn.com:*");
 BrowserPolicy.content.allowOriginForAll("https://static.intercomassets.com:*");
 BrowserPolicy.content.allowOriginForAll("https://aktively.us3.list-manage.com:*");
 BrowserPolicy.content.allowOriginForAll("https://fr-assets.com:*");
+BrowserPolicy.content.allowOriginForAll("https://chimpstatic.com:*");
 
 const absoluteUrl = Meteor.absoluteUrl().slice(0, -1);
 if (absoluteUrl.includes('localhost') === false) {
